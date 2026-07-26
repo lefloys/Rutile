b193216a033cfd3872e698739e1a1ed177b93984
lefloysi <lefloysi@gmail.com>
2026-07-26 20:36:02 +0200

    ...

--- runtime/backends/rt-vulkan/src/glfw/swapchain.h
+++ runtime/backends/rt-vulkan/src/glfw/swapchain.h
@@ -9,6 +9,9 @@
 
 RTVK_API void rtSwapchainBindWindowGLFW(rt_swapchain swapchain, GLFWwindow* window);
 
+struct rtvk_context;
+struct rtvk_swapchain;
+
 void rtvk_swapchain_bind_window_glfw(struct rtvk_context* ctx, struct rtvk_swapchain* swapchain, GLFWwindow* window);
 
 #endif /* RTVK_GLFW_SWAPCHAIN_H */
